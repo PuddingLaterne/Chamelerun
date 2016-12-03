22c84118ae5008c5436ed952dc55b9c40f6aab4b
Stella <puddinglaterne@googlemail.com>
2016-12-03 14:27:15 +0100

    fake shadows

---
+++ Chamelerun/Assets/Scripts/General/Shadow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shadow : MonoBehaviour
+{
+    public GameObject ShadowCaster;
+
+    public LayerMask ShadowReceivingLayers;
+    public float MaxShadowDistance = 2;
+
+    public int MaxAdditionalRaycasts = 6;
+    public float AdditionalRaycastFraction = 0.8f;
+
+    public void Update()
+    {
+        Vector2 origin = ShadowCaster.transform.position;
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(origin, Vector2.down, MaxShadowDistance, ShadowReceivingLayers);
+        if(hit)
+        {
+            float scale = 1 - (hit.distance / MaxShadowDistance);
+
+            float extentsLeft = GetClosestGroundPosition(origin, hit.distance, -scale / 2f) * (-1);
+            float extensRight = GetClosestGroundPosition(origin, hit.distance, scale / 2f);
+            
+            float posX = hit.point.x - (extentsLeft / 2f) + (extensRight / 2f);
+
+            transform.localScale = new Vector2(extensRight + extentsLeft, scale);
+            transform.position = new Vector2(posX, hit.point.y);
+        }
+        else
+        {
+            transform.localScale = Vector2.zero;
+        }
+    }
+
+    private float GetClosestGroundPosition(Vector2 origin, float distance, float width)
+    {
+        int numRaycasts = 0;
+        float closestGroundPos = -1;
+        while (closestGroundPos == -1)
+        {
+            if (Physics2D.Raycast(origin + new Vector2(width, 0), Vector2.down, distance, ShadowReceivingLayers))
+            {
+                closestGroundPos = width;
+            }
+            else
+            {
+                width = width * AdditionalRaycastFraction;
+            }
+            numRaycasts++;
+            if (numRaycasts > MaxAdditionalRaycasts)
+            {
+                closestGroundPos = 0;
+            }
+        }
+        return closestGroundPos;
+    }
+
+}
